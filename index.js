e923a764978be9aff1b55afaa9014db7ad752464
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-06-23 19:51:17 -0500

    pokemon final

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 "use strict";
 
-const assert = require('assert');
+let player1Exp = 0;
+let player2Exp = 0;
 
-const sum = arr => arr.reduce((acc,val)=>acc+val,0);
+const thePokemon = [];
 
-const getPosts = (fetch,id) => {
-  return fetch("https://pokeapi.co/docs/v2.html/#pokemon1/"+id)
+const getRandomIntInclusive = (min, max)=>{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random()*(max-min+1))+ min;
+}
+const randomPokemon = getRandomIntInclusive(1, 250);
+const randomPokemon2 = getRandomIntInclusive(251, 500);
+
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
+.then(res => res.json())
+.then((data)=> {
+  thePokemon.push(data);
+  const pokemon = document.getElementById("pokemon");
+  pokemon.src = data.sprites.front_default;
+  player1Exp = data.base_experience;
+})
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon2}`)
+.then(res => res.json())
+.then((data)=> {
+  thePokemon.push(data);
+  const pokemon = document.getElementById("pokemon2");
+  pokemon.src = data.sprites.front_default;
+  player2Exp = data.base_experience;
+})
+
+const compareExp = (exp1, exp2) => {
+  const winner = document.getElementById("winner");
+    if(exp1 > exp2) {
+      winner.src = thePokemon[0].sprites.front_default
+    } else {
+      winner.src = thePokemon[1].sprites.front_default
+    }
 }
 
-//tests
-
-if (typeof describe === "function") {
-  describe("#Test fetch", () => {
-    let count = 0;
-
-    const fakeFetch = (url) =>
-    {
-      const arr=url.split('/');
-      let id=Number(arr[arr.length-1]);
-      if ( id<=0 || id >30)
-      return "Out of range"
-      else
-      return url;
-    }
-    it("Miss typed address", () => {
-      assert.equal(getPosts(fakeFetch,15), "https://pokeapi.co/docs/v2.html/#pokemon1/15");
-    });
-    it("Out of range",()=>{
-      assert.equal(getPosts(fakeFetch,31),"Out of range")
-    })
-    })
-  } else {
-  
-    console.log("Only run the test on this one")
-  }
+setTimeout(() => {
+  return compareExp(player1Exp, player2Exp)
+}, 3000)
